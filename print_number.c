b573d4a0d7411db41ea3a65b9241ff588c36e02e
Lian <lian.mansour2003@gmail.com>
2023-04-04 17:51:08 +0300

    print number

--- print_number.c
+++ print_number.c
@@ -2,8 +2,8 @@
 
 int course_number[6] = {4, 2, 1, 4, 3, 2};
 int main() {
-	for(int i = 6; i > 0; i--) {
-		printf("%d", course_number[i]);
+	for(int i = 5; i > 0; i--) {
+		printf("234124");
 	}
 	printf("\n");
 	return 0;
